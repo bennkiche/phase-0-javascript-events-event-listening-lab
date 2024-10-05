c171636b546031234a31d331bc641895414427df
Benn <bennkiche7@gmail.com>
2024-10-05 22:55:30 +0300

    submit task

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+
 function addingEventListener() {
-}
+    const button = document.getElementById('button');
+    button.addEventListener('click', function() {
+      // Define what happens when the button is clicked
+      console.log('Button was clicked!');
+    });
+  }
+  
+  // Call the function to bind the event listener
+  addingEventListener();
+  
